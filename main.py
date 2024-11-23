f8d9e8d1ccb39727f1698c2cb898b8ff778cbe2b
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-11-24 00:49:58 +0800

    feat : lots of features
    * 因 Google Spread Sheets API 有每分鐘次數上限，故將原本 `update_value` 改為批量 `update_value_batch`
    * 將時間設為 30 分鐘
    * 因使用 `update_value_batch` 修改部分檔案架構

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pygsheets
 import discord
 import datetime
@@ -10,6 +12,8 @@ webhook_url = "WEBHOOK_URL_HERE"
 
 testcase_list = ["前端測試紀錄 01"]
 testcase_handler = "常數表"
+toCheck = []
+toTime = []
 
 
 
@@ -33,19 +37,21 @@ while 1:
 
         for testcase in range(1 , len(current)):
 
-            print(f"[Checking] testcase {testcase}")
-
             name = current[testcase]['通知 @']
-            if current[testcase]['已通知'] == "FALSE" and len(name) == 3:
-                cur.update_value(f"N{testcase + 2}" , "TRUE")
-                cur.update_value(f"O{testcase + 2}" , datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+            if current[testcase]['已通知'] == "" and len(name) == 3 and current[testcase]["For 開發人員 已修復"] == "FALSE":
+                toCheck.append((f"N{testcase + 2}:N{testcase + 2}"))
+                toTime.append(f"O{testcase + 2}:O{testcase + 2}")
                 people[name].append(testcase)
+
+                print(f"[Found] testcase {testcase}")
                 
 
         print("[Done] Scan testcase")
 
         
 
+        cur.update_values_batch(toCheck , [[["TRUE"]] for i in range(len(toCheck))])
+        cur.update_values_batch(toTime , [[[datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")]] for i in range(len(toTime))])
 
         for person in range(len(handler)):
 
@@ -57,7 +63,7 @@ while 1:
             data = {
                 "username":"軟工測試報告通知",
                 "avatar_url": "https://www.shutterstock.com/image-vector/chat-bot-icon-virtual-smart-600nw-2478937555.jpg",
-                "content":f"<@{handler[person]["Discord User ID"]}>"
+                "content":f"<@{handler[person]['Discord User ID']}>"
                 }
 
             data["embeds"] = [
@@ -75,9 +81,11 @@ while 1:
             for field in people[handler[person]["組員"]]:
 
                 data['embeds'][0]['fields'].append({
-                    "name":f"{current[field]['target']} : {current[field]["測試內容"]}",
-                    "value":f"{current[field]["測試結果"]}"
+                    "name":f"{current[field]['target']} : {current[field]['測試內容']}",
+                    "value":f"{current[field]['測試結果']}"
                 })
+
+                print(f" [Append] {current[field]['測試內容']}")
 
             result = requests.post(webhook_url, json=data)
             print(f"[Webhook]")
@@ -87,4 +95,4 @@ while 1:
     print("[Done] All Pages Have been searched")
 
 
-    time.sleep(60)
+    time.sleep(1800)
